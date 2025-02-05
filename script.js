4c31ec09e48cc74d38a967835130ad1817383e7a
Shouvanik21 <shouvanik210305@gmail.com>
2025-02-05 23:52:47 +0530

    updated commit

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const API_URL="https://api.github.com/users/";
 
 const main = document.getElementById("main");
 const searchBox=document.getElementById("search");
+
+const getUser = async (username) => {
+    try{
+        const response = await fetch(API_URL + username);
+        const data = await response.json();
+        const card = 
+        <div class="card">
+            <div>
+                <img src="${data.avatar}" alt="" />
+            </div>
+        </div>
+        
+    }
+}
